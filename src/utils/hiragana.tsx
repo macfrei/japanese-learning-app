249dd699252796863290f0bb0e053037923672c5
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-03-29 16:42:21 +0900

    Add data for hiragana and katakana

---
+++ src/utils/hiragana.tsx
@@ -0,0 +1,530 @@
+type kana = {
+  id: string;
+  pronunciation: string;
+  kana: string;
+};
+
+const hiragana: kana[] = [
+  {
+    id: 'h_a',
+    pronunciation: 'A',
+    kana: 'あ',
+  },
+  {
+    id: 'h_i',
+    pronunciation: 'I',
+    kana: 'い',
+  },
+  {
+    id: 'h_u',
+    pronunciation: 'U',
+    kana: 'う',
+  },
+  {
+    id: 'h_e',
+    pronunciation: 'E',
+    kana: 'え',
+  },
+  {
+    id: 'h_o',
+    pronunciation: 'O',
+    kana: 'お',
+  },
+  {
+    id: 'h_ka',
+    pronunciation: 'KA',
+    kana: 'か',
+  },
+  {
+    id: 'h_ki',
+    pronunciation: 'KI',
+    kana: 'き',
+  },
+  {
+    id: 'h_ku',
+    pronunciation: 'KU',
+    kana: 'く',
+  },
+  {
+    id: 'h_ke',
+    pronunciation: 'KE',
+    kana: 'け',
+  },
+  {
+    id: 'h_ko',
+    pronunciation: 'KO',
+    kana: 'こ',
+  },
+  {
+    id: 'h_sa',
+    pronunciation: 'SA',
+    kana: 'さ',
+  },
+  {
+    id: 'h_shi',
+    pronunciation: 'SHI',
+    kana: 'し',
+  },
+  {
+    id: 'h_su',
+    pronunciation: 'SU',
+    kana: 'す',
+  },
+  {
+    id: 'h_se',
+    pronunciation: 'SE',
+    kana: 'せ',
+  },
+  {
+    id: 'h_so',
+    pronunciation: 'SO',
+    kana: 'そ',
+  },
+  {
+    id: 'h_ta',
+    pronunciation: 'TA',
+    kana: 'た',
+  },
+  {
+    id: 'h_chi',
+    pronunciation: 'CHI',
+    kana: 'ち',
+  },
+  {
+    id: 'h_tsu',
+    pronunciation: 'TSU',
+    kana: 'つ',
+  },
+  {
+    id: 'h_te',
+    pronunciation: 'TE',
+    kana: 'て',
+  },
+  {
+    id: 'h_to',
+    pronunciation: 'TO',
+    kana: 'と',
+  },
+  {
+    id: 'h_na',
+    pronunciation: 'NA',
+    kana: 'な',
+  },
+  {
+    id: 'h_ni',
+    pronunciation: 'NI',
+    kana: 'に',
+  },
+  {
+    id: 'h_nu',
+    pronunciation: 'NU',
+    kana: 'ぬ',
+  },
+  {
+    id: 'h_ne',
+    pronunciation: 'NE',
+    kana: 'ね',
+  },
+  {
+    id: 'h_no',
+    pronunciation: 'NO',
+    kana: 'の',
+  },
+  {
+    id: 'h_ha',
+    pronunciation: 'HA',
+    kana: 'は',
+  },
+  {
+    id: 'h_hi',
+    pronunciation: 'HI',
+    kana: 'ひ',
+  },
+  {
+    id: 'h_fu',
+    pronunciation: 'FU',
+    kana: 'ふ',
+  },
+  {
+    id: 'h_he',
+    pronunciation: 'HE',
+    kana: 'へ',
+  },
+  {
+    id: 'h_ho',
+    pronunciation: 'HO',
+    kana: 'ほ',
+  },
+  {
+    id: 'h_ma',
+    pronunciation: 'MA',
+    kana: 'ま',
+  },
+  {
+    id: 'h_mi',
+    pronunciation: 'MI',
+    kana: 'み',
+  },
+  {
+    id: 'h_mu',
+    pronunciation: 'MU',
+    kana: 'む',
+  },
+  {
+    id: 'h_me',
+    pronunciation: 'ME',
+    kana: 'め',
+  },
+  {
+    id: 'h_mo',
+    pronunciation: 'MO',
+    kana: 'も',
+  },
+  {
+    id: 'h_ya',
+    pronunciation: 'YA',
+    kana: 'や',
+  },
+  {
+    id: 'h_yu',
+    pronunciation: 'YU',
+    kana: 'ゆ',
+  },
+  {
+    id: 'h_yo',
+    pronunciation: 'YO',
+    kana: 'よ',
+  },
+  {
+    id: 'h_ra',
+    pronunciation: 'RA',
+    kana: 'ら',
+  },
+  {
+    id: 'h_ri',
+    pronunciation: 'RI',
+    kana: 'り',
+  },
+  {
+    id: 'h_ru',
+    pronunciation: 'RU',
+    kana: 'る',
+  },
+  {
+    id: 'h_re',
+    pronunciation: 'RE',
+    kana: 'れ',
+  },
+  {
+    id: 'h_ro',
+    pronunciation: 'RO',
+    kana: 'ろ',
+  },
+  {
+    id: 'h_wa',
+    pronunciation: 'WA',
+    kana: 'わ',
+  },
+  {
+    id: 'h_wo',
+    pronunciation: 'WO',
+    kana: 'を',
+  },
+  {
+    id: 'h_n',
+    pronunciation: 'N',
+    kana: 'ん',
+  },
+  {
+    id: 'h_ga',
+    pronunciation: 'GA',
+    kana: 'が',
+  },
+  {
+    id: 'h_gi',
+    pronunciation: 'GI',
+    kana: 'ぎ',
+  },
+  {
+    id: 'h_gu',
+    pronunciation: 'GU',
+    kana: 'ぐ',
+  },
+  {
+    id: 'h_ge',
+    pronunciation: 'GE',
+    kana: 'げ',
+  },
+  {
+    id: 'h_go',
+    pronunciation: 'GO',
+    kana: 'ご',
+  },
+  {
+    id: 'h_za',
+    pronunciation: 'ZA',
+    kana: 'ざ',
+  },
+  {
+    id: 'h_zi',
+    pronunciation: 'ZI',
+    kana: 'じ',
+  },
+  {
+    id: 'h_zu',
+    pronunciation: 'ZU',
+    kana: 'ず',
+  },
+  {
+    id: 'h_ze',
+    pronunciation: 'ZE',
+    kana: 'ぜ',
+  },
+  {
+    id: 'h_zo',
+    pronunciation: 'ZO',
+    kana: 'ぞ',
+  },
+  {
+    id: 'h_da',
+    pronunciation: 'DA',
+    kana: 'だ',
+  },
+  {
+    id: 'h_di',
+    pronunciation: 'DI',
+    kana: 'ぢ',
+  },
+  {
+    id: 'h_du',
+    pronunciation: 'DU',
+    kana: 'づ',
+  },
+  {
+    id: 'h_de',
+    pronunciation: 'DE',
+    kana: 'で',
+  },
+  {
+    id: 'h_do',
+    pronunciation: 'DO',
+    kana: 'ど',
+  },
+  {
+    id: 'h_ba',
+    pronunciation: 'BA',
+    kana: 'ば',
+  },
+  {
+    id: 'h_bi',
+    pronunciation: 'BI',
+    kana: 'び',
+  },
+  {
+    id: 'h_bu',
+    pronunciation: 'BU',
+    kana: 'ぶ',
+  },
+  {
+    id: 'h_be',
+    pronunciation: 'BE',
+    kana: 'べ',
+  },
+  {
+    id: 'h_bo',
+    pronunciation: 'BO',
+    kana: 'ぼ',
+  },
+  {
+    id: 'h_pa',
+    pronunciation: 'PA',
+    kana: 'ぱ',
+  },
+  {
+    id: 'h_pi',
+    pronunciation: 'PI',
+    kana: 'ぴ',
+  },
+  {
+    id: 'h_pu',
+    pronunciation: 'PU',
+    kana: 'ぷ',
+  },
+  {
+    id: 'h_pe',
+    pronunciation: 'PE',
+    kana: 'ぺ',
+  },
+  {
+    id: 'h_po',
+    pronunciation: 'PO',
+    kana: 'ぽ',
+  },
+  {
+    id: 'h_kya',
+    pronunciation: 'KYA',
+    kana: 'きゃ',
+  },
+  {
+    id: 'h_kyu',
+    pronunciation: 'KYU',
+    kana: 'きゅ',
+  },
+  {
+    id: 'h_kyo',
+    pronunciation: 'KYO',
+    kana: 'きょ',
+  },
+  {
+    id: 'h_sha',
+    pronunciation: 'SHA',
+    kana: 'しゃ',
+  },
+  {
+    id: 'h_shu',
+    pronunciation: 'SHU',
+    kana: 'しゅ',
+  },
+  {
+    id: 'h_sho',
+    pronunciation: 'SHO',
+    kana: 'しょ',
+  },
+  {
+    id: 'h_cha',
+    pronunciation: 'CHA',
+    kana: 'ちゃ',
+  },
+  {
+    id: 'h_chu',
+    pronunciation: 'CHU',
+    kana: 'ちゅ',
+  },
+  {
+    id: 'h_cho',
+    pronunciation: 'CHO',
+    kana: 'ちょ',
+  },
+  {
+    id: 'h_nya',
+    pronunciation: 'NYA',
+    kana: 'にゃ',
+  },
+  {
+    id: 'h_nyu',
+    pronunciation: 'NYU',
+    kana: 'にゅ',
+  },
+  {
+    id: 'h_nyo',
+    pronunciation: 'NYO',
+    kana: 'にょ',
+  },
+  {
+    id: 'h_hya',
+    pronunciation: 'HYA',
+    kana: 'ひゃ',
+  },
+  {
+    id: 'h_hyu',
+    pronunciation: 'HYU',
+    kana: 'ひゅ',
+  },
+  {
+    id: 'h_hyo',
+    pronunciation: 'HYO',
+    kana: 'ひょ',
+  },
+  {
+    id: 'h_mya',
+    pronunciation: 'MYA',
+    kana: 'みゃ',
+  },
+  {
+    id: 'h_myu',
+    pronunciation: 'MYU',
+    kana: 'みゅ',
+  },
+  {
+    id: 'h_myo',
+    pronunciation: 'MYO',
+    kana: 'みょ',
+  },
+  {
+    id: 'h_rya',
+    pronunciation: 'RYA',
+    kana: 'りゃ',
+  },
+  {
+    id: 'h_ryu',
+    pronunciation: 'RYU',
+    kana: 'りゅ',
+  },
+  {
+    id: 'h_ryo',
+    pronunciation: 'RYO',
+    kana: 'りょ',
+  },
+  {
+    id: 'h_gya',
+    pronunciation: 'GYA',
+    kana: 'ぎゃ',
+  },
+  {
+    id: 'h_gyu',
+    pronunciation: 'GYU',
+    kana: 'ぎゅ',
+  },
+  {
+    id: 'h_gyo',
+    pronunciation: 'GYO',
+    kana: 'ぎょ',
+  },
+  {
+    id: 'h_ja',
+    pronunciation: 'JA',
+    kana: 'じゃ',
+  },
+  {
+    id: 'h_ju',
+    pronunciation: 'JU',
+    kana: 'じゅ',
+  },
+  {
+    id: 'h_jo',
+    pronunciation: 'JO',
+    kana: 'じょ',
+  },
+  {
+    id: 'h_bya',
+    pronunciation: 'BYA',
+    kana: 'びゃ',
+  },
+  {
+    id: 'h_byu',
+    pronunciation: 'BYU',
+    kana: 'びゅ',
+  },
+  {
+    id: 'h_byo',
+    pronunciation: 'BYO',
+    kana: 'びょ',
+  },
+  {
+    id: 'h_pya',
+    pronunciation: 'PYA',
+    kana: 'ぴゃ',
+  },
+  {
+    id: 'h_pyu',
+    pronunciation: 'PYU',
+    kana: 'ぴゅ',
+  },
+  {
+    id: 'h_pyo',
+    pronunciation: 'PYO',
+    kana: 'ぴょ',
+  },
+];
+
+export default hiragana;
---
+++ src/utils/katakana.tsx
@@ -0,0 +1,530 @@
+type kana = {
+  id: string;
+  pronunciation: string;
+  kana: string;
+};
+
+const katakana: kana[] = [
+  {
+    id: 'k_a',
+    pronunciation: 'A',
+    kana: 'ア',
+  },
+  {
+    id: 'k_i',
+    pronunciation: 'I',
+    kana: 'イ',
+  },
+  {
+    id: 'k_u',
+    pronunciation: 'U',
+    kana: 'ウ',
+  },
+  {
+    id: 'k_e',
+    pronunciation: 'E',
+    kana: 'エ',
+  },
+  {
+    id: 'k_o',
+    pronunciation: 'O',
+    kana: 'オ',
+  },
+  {
+    id: 'k_ka',
+    pronunciation: 'KA',
+    kana: 'カ',
+  },
+  {
+    id: 'k_ki',
+    pronunciation: 'KI',
+    kana: 'キ',
+  },
+  {
+    id: 'k_ku',
+    pronunciation: 'KU',
+    kana: 'ク',
+  },
+  {
+    id: 'k_ke',
+    pronunciation: 'KE',
+    kana: 'ケ',
+  },
+  {
+    id: 'k_ko',
+    pronunciation: 'KO',
+    kana: 'コ',
+  },
+  {
+    id: 'k_sa',
+    pronunciation: 'SA',
+    kana: 'サ',
+  },
+  {
+    id: 'k_shi',
+    pronunciation: 'SHI',
+    kana: 'シ',
+  },
+  {
+    id: 'k_su',
+    pronunciation: 'SU',
+    kana: 'ス',
+  },
+  {
+    id: 'k_se',
+    pronunciation: 'SE',
+    kana: 'セ',
+  },
+  {
+    id: 'k_so',
+    pronunciation: 'SO',
+    kana: 'ソ',
+  },
+  {
+    id: 'k_ta',
+    pronunciation: 'TA',
+    kana: 'タ',
+  },
+  {
+    id: 'k_chi',
+    pronunciation: 'CHI',
+    kana: 'チ',
+  },
+  {
+    id: 'k_tsu',
+    pronunciation: 'TSU',
+    kana: 'ツ',
+  },
+  {
+    id: 'k_te',
+    pronunciation: 'TE',
+    kana: 'テ',
+  },
+  {
+    id: 'k_to',
+    pronunciation: 'TO',
+    kana: 'ト',
+  },
+  {
+    id: 'k_na',
+    pronunciation: 'NA',
+    kana: 'ナ',
+  },
+  {
+    id: 'k_ni',
+    pronunciation: 'NI',
+    kana: 'ニ',
+  },
+  {
+    id: 'k_nu',
+    pronunciation: 'NU',
+    kana: 'ヌ',
+  },
+  {
+    id: 'k_ne',
+    pronunciation: 'NE',
+    kana: 'ネ',
+  },
+  {
+    id: 'k_no',
+    pronunciation: 'NO',
+    kana: 'ノ',
+  },
+  {
+    id: 'k_ha',
+    pronunciation: 'HA',
+    kana: 'ハ',
+  },
+  {
+    id: 'k_hi',
+    pronunciation: 'HI',
+    kana: 'ヒ',
+  },
+  {
+    id: 'k_fu',
+    pronunciation: 'FU',
+    kana: 'フ',
+  },
+  {
+    id: 'k_he',
+    pronunciation: 'HE',
+    kana: 'ヘ',
+  },
+  {
+    id: 'k_ho',
+    pronunciation: 'HO',
+    kana: 'ホ',
+  },
+  {
+    id: 'k_ma',
+    pronunciation: 'MA',
+    kana: 'マ',
+  },
+  {
+    id: 'k_mi',
+    pronunciation: 'MI',
+    kana: 'ミ',
+  },
+  {
+    id: 'k_mu',
+    pronunciation: 'MU',
+    kana: 'ム',
+  },
+  {
+    id: 'k_me',
+    pronunciation: 'ME',
+    kana: 'メ',
+  },
+  {
+    id: 'k_mo',
+    pronunciation: 'MO',
+    kana: 'モ',
+  },
+  {
+    id: 'k_ya',
+    pronunciation: 'YA',
+    kana: 'ヤ',
+  },
+  {
+    id: 'k_yu',
+    pronunciation: 'YU',
+    kana: 'ユ',
+  },
+  {
+    id: 'k_yo',
+    pronunciation: 'YO',
+    kana: 'ヨ',
+  },
+  {
+    id: 'k_ra',
+    pronunciation: 'RA',
+    kana: 'ラ',
+  },
+  {
+    id: 'k_ri',
+    pronunciation: 'RI',
+    kana: 'リ',
+  },
+  {
+    id: 'k_ru',
+    pronunciation: 'RU',
+    kana: 'ル',
+  },
+  {
+    id: 'k_re',
+    pronunciation: 'RE',
+    kana: 'レ',
+  },
+  {
+    id: 'k_ro',
+    pronunciation: 'RO',
+    kana: 'ロ',
+  },
+  {
+    id: 'k_wa',
+    pronunciation: 'WA',
+    kana: 'ワ',
+  },
+  {
+    id: 'k_wo',
+    pronunciation: 'WO',
+    kana: 'ヲ',
+  },
+  {
+    id: 'k_n',
+    pronunciation: 'N',
+    kana: 'ン',
+  },
+  {
+    id: 'k_ga',
+    pronunciation: 'GA',
+    kana: 'ガ',
+  },
+  {
+    id: 'k_gi',
+    pronunciation: 'GI',
+    kana: 'ギ',
+  },
+  {
+    id: 'k_gu',
+    pronunciation: 'GU',
+    kana: 'グ',
+  },
+  {
+    id: 'k_ge',
+    pronunciation: 'GE',
+    kana: 'ゲ',
+  },
+  {
+    id: 'k_go',
+    pronunciation: 'GO',
+    kana: 'ゴ',
+  },
+  {
+    id: 'k_za',
+    pronunciation: 'ZA',
+    kana: 'ザ',
+  },
+  {
+    id: 'k_zi',
+    pronunciation: 'ZI',
+    kana: 'ジ',
+  },
+  {
+    id: 'k_zu',
+    pronunciation: 'ZU',
+    kana: 'ズ',
+  },
+  {
+    id: 'k_ze',
+    pronunciation: 'ZE',
+    kana: 'ゼ',
+  },
+  {
+    id: 'k_zo',
+    pronunciation: 'ZO',
+    kana: 'ゾ',
+  },
+  {
+    id: 'k_da',
+    pronunciation: 'DA',
+    kana: 'ダ',
+  },
+  {
+    id: 'k_di',
+    pronunciation: 'DI',
+    kana: 'ヂ',
+  },
+  {
+    id: 'k_du',
+    pronunciation: 'DU',
+    kana: 'ヅ',
+  },
+  {
+    id: 'k_de',
+    pronunciation: 'DE',
+    kana: 'デ',
+  },
+  {
+    id: 'k_do',
+    pronunciation: 'DO',
+    kana: 'ド',
+  },
+  {
+    id: 'k_ba',
+    pronunciation: 'BA',
+    kana: 'バ',
+  },
+  {
+    id: 'k_bi',
+    pronunciation: 'BI',
+    kana: 'ビ',
+  },
+  {
+    id: 'k_bu',
+    pronunciation: 'BU',
+    kana: 'ブ',
+  },
+  {
+    id: 'k_be',
+    pronunciation: 'BE',
+    kana: 'ベ',
+  },
+  {
+    id: 'k_bo',
+    pronunciation: 'BO',
+    kana: 'ボ',
+  },
+  {
+    id: 'k_pa',
+    pronunciation: 'PA',
+    kana: 'パ',
+  },
+  {
+    id: 'k_pi',
+    pronunciation: 'PI',
+    kana: 'ピ',
+  },
+  {
+    id: 'k_pu',
+    pronunciation: 'PU',
+    kana: 'プ',
+  },
+  {
+    id: 'k_pe',
+    pronunciation: 'PE',
+    kana: 'ペ',
+  },
+  {
+    id: 'k_po',
+    pronunciation: 'PO',
+    kana: 'ポ',
+  },
+  {
+    id: 'k_kya',
+    pronunciation: 'KYA',
+    kana: 'キャ',
+  },
+  {
+    id: 'k_kyu',
+    pronunciation: 'KYU',
+    kana: 'キュ',
+  },
+  {
+    id: 'k_kyo',
+    pronunciation: 'KYO',
+    kana: 'キョ',
+  },
+  {
+    id: 'k_sha',
+    pronunciation: 'SHA',
+    kana: 'シャ',
+  },
+  {
+    id: 'k_shu',
+    pronunciation: 'SHU',
+    kana: 'シュ',
+  },
+  {
+    id: 'k_sho',
+    pronunciation: 'SHO',
+    kana: 'ショ',
+  },
+  {
+    id: 'k_cha',
+    pronunciation: 'CHA',
+    kana: 'チャ',
+  },
+  {
+    id: 'k_chu',
+    pronunciation: 'CHU',
+    kana: 'チュ',
+  },
+  {
+    id: 'k_cho',
+    pronunciation: 'CHO',
+    kana: 'チョ',
+  },
+  {
+    id: 'k_nya',
+    pronunciation: 'NYA',
+    kana: 'ニャ',
+  },
+  {
+    id: 'k_nyu',
+    pronunciation: 'NYU',
+    kana: 'ニュ',
+  },
+  {
+    id: 'k_nyo',
+    pronunciation: 'NYO',
+    kana: 'ニョ',
+  },
+  {
+    id: 'k_hya',
+    pronunciation: 'HYA',
+    kana: 'ヒャ',
+  },
+  {
+    id: 'k_hyu',
+    pronunciation: 'HYU',
+    kana: 'ヒュ',
+  },
+  {
+    id: 'k_hyo',
+    pronunciation: 'HYO',
+    kana: 'ヒョ',
+  },
+  {
+    id: 'k_mya',
+    pronunciation: 'MYA',
+    kana: 'ミャ',
+  },
+  {
+    id: 'k_myu',
+    pronunciation: 'MYU',
+    kana: 'ミュ',
+  },
+  {
+    id: 'k_myo',
+    pronunciation: 'MYO',
+    kana: 'ミョ',
+  },
+  {
+    id: 'k_rya',
+    pronunciation: 'RYA',
+    kana: 'リャ',
+  },
+  {
+    id: 'k_ryu',
+    pronunciation: 'RYU',
+    kana: 'リュ',
+  },
+  {
+    id: 'k_ryo',
+    pronunciation: 'RYO',
+    kana: 'リョ',
+  },
+  {
+    id: 'k_gya',
+    pronunciation: 'GYA',
+    kana: 'ガャ',
+  },
+  {
+    id: 'k_gyu',
+    pronunciation: 'GYU',
+    kana: 'ガュ',
+  },
+  {
+    id: 'k_gyo',
+    pronunciation: 'GYO',
+    kana: 'ガョ',
+  },
+  {
+    id: 'k_ja',
+    pronunciation: 'JA',
+    kana: 'ジャ',
+  },
+  {
+    id: 'k_ju',
+    pronunciation: 'JU',
+    kana: 'ジュ',
+  },
+  {
+    id: 'k_jo',
+    pronunciation: 'JO',
+    kana: 'ジョ',
+  },
+  {
+    id: 'k_bya',
+    pronunciation: 'BYA',
+    kana: 'ビャ',
+  },
+  {
+    id: 'k_byu',
+    pronunciation: 'BYU',
+    kana: 'ビュ',
+  },
+  {
+    id: 'k_byo',
+    pronunciation: 'BYO',
+    kana: 'ビョ',
+  },
+  {
+    id: 'k_pya',
+    pronunciation: 'PYA',
+    kana: 'ポャ',
+  },
+  {
+    id: 'k_pyu',
+    pronunciation: 'PYU',
+    kana: 'ポュ',
+  },
+  {
+    id: 'k_pyo',
+    pronunciation: 'PYO',
+    kana: 'ポョ',
+  },
+];
+
+export default katakana;
