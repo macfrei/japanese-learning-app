a2acaa95bf0b086688fe79ff422dba5c5c8e1995
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-03-30 10:45:13 +0900

    Use KanaTable component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import hiragana from './assets/hiragana';
+import katakana from './assets/katakana';
+import KanaTable from './components/KanaTable';
+
 function App() {
-  return <div>Japanese Learning App</div>;
+  const basicHiragana = hiragana.filter(({ type }) => type === 'basic');
+  const basicKatakana = katakana.filter(({ type }) => type === 'basic');
+  return (
+    <div>
+      <KanaTable kana={basicHiragana} caption="Basic Hiragana Table" />
+      <KanaTable kana={basicKatakana} caption="Basic Katakana Table" />
+    </div>
+  );
 }
 
 export default App;
