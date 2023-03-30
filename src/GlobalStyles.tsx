8bd94d402ce091cebc6e3d7ad4aecf971c8650bc
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-03-30 10:52:45 +0900

    Add GlobalStyles

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+*,
+*:before,
+*:after {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    font-family: sans-serif;
+    line-height: 1.4;
+}
+
+button, input {
+    font-size: inherit;
+}
+`;
