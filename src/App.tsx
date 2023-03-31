fc49d4c5c4af2451d0b65a5e8a84391d8a1f263c
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-03-31 11:26:47 +0900

    Add kana tables to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import hiragana from './assets/hiragana';
 import katakana from './assets/katakana';
-import KanaTable from './components/KanaTable/KanaTable';
+import BasicKanaTable from './components/KanaTable/BasicKanaTable';
+import TenTenKanaTable from './components/KanaTable/TenTenKanaTable';
+import YoonKanaTable from './components/KanaTable/YoonKanaTable';
 
 function App() {
   const basicHiragana = hiragana.filter(({ type }) => type === 'basic');
   const basicKatakana = katakana.filter(({ type }) => type === 'basic');
+
+  const tentenHiragana = hiragana.filter(({ type }) => type === 'tenten');
+  const tentenKatakana = katakana.filter(({ type }) => type === 'tenten');
+
+  const yoonHiragana = hiragana.filter(({ type }) => type === 'yoon');
+  const yoonKatakana = katakana.filter(({ type }) => type === 'yoon');
   return (
     <div>
-      <KanaTable kana={basicHiragana} caption="Basic Hiragana Table" />
-      <KanaTable kana={basicKatakana} caption="Basic Katakana Table" />
+      <BasicKanaTable kana={basicHiragana} caption="Basic Hiragana Table" />
+      <BasicKanaTable kana={basicKatakana} caption="Basic Katakana Table" />
+      <TenTenKanaTable kana={tentenHiragana} caption="Tenten Hiragana Table" />
+      <TenTenKanaTable kana={tentenKatakana} caption="Tenten Katakana Table" />
+      <YoonKanaTable kana={yoonHiragana} caption="Yoon Hiragana Table" />
+      <YoonKanaTable kana={yoonKatakana} caption="Yoon Katakana Table" />
     </div>
   );
 }
