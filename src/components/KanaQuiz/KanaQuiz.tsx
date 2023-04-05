05fa0d76efef6ee57400b3ebb10fbc1b584a4de0
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-04-05 11:23:18 +0900

    Add basic quiz styles (mobile first)

--- src/components/KanaQuiz/KanaQuiz.tsx
+++ src/components/KanaQuiz/KanaQuiz.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import useKanaQuizList from '../../hooks/useKanaQuizList';
 import { KanaType } from '../../types/kana';
 import Question from './Question';
 
 type KanaQuizProps = {
   kana: KanaType[];
+};
+
+type FeedbackProps = {
+  isRight: boolean;
 };
 
 export default function KanaQuiz({ kana }: KanaQuizProps) {
@@ -24,19 +29,52 @@ export default function KanaQuiz({ kana }: KanaQuizProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Question currentQuestion={currentQuestion} onChange={checkAnswer} />
-      {feedback.length > 0 && <p>{feedback}</p>}
       {quizKanaLength > 1 && (
-        <button type="button" onClick={getNewQuestion} disabled={isDisabled}>
+        <Button type="button" onClick={getNewQuestion} disabled={isDisabled}>
           Next
-        </button>
+        </Button>
       )}
       {quizKanaLength === 1 && (
-        <button type="submit" disabled={isDisabled}>
+        <Button type="submit" disabled={isDisabled}>
           Result
-        </button>
+        </Button>
       )}
-    </form>
+      {feedback.length > 0 && (
+        <Feedback isRight={!isDisabled}>{feedback}</Feedback>
+      )}
+    </Form>
   );
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-template-rows: 1fr 45px 45px;
+  align-content: start;
+  gap: 24px;
+  padding: 12px;
+  height: 100vh;
+`;
+
+const Feedback = styled.p<FeedbackProps>`
+  color: ${props => (props.isRight ? 'green' : 'red')};
+  font-style: italic;
+  margin: 0;
+  display: grid;
+  place-items: center;
+`;
+
+const Button = styled.button`
+  font-size: inherit;
+  background-color: white;
+  color: black;
+  border: 1px solid;
+  border-radius: 8px;
+  padding: 12px;
+
+  &:disabled {
+    color: lightgray;
+    border-color: lightgray;
+  }
+`;
--- src/components/KanaQuiz/Question.tsx
+++ src/components/KanaQuiz/Question.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Answer, CurrentQuestion } from '../../types/kanaQuiz';
 
 type QuestionProps = {
@@ -7,7 +8,7 @@ type QuestionProps = {
 
 export default function Question({ currentQuestion, onChange }: QuestionProps) {
   return (
-    <fieldset>
+    <QuestionStyled>
       <legend>{currentQuestion.question?.kana}</legend>
       {currentQuestion.answers.map(answer => (
         <label key={answer?.id}>
@@ -20,6 +21,54 @@ export default function Question({ currentQuestion, onChange }: QuestionProps) {
           />
         </label>
       ))}
-    </fieldset>
+    </QuestionStyled>
   );
 }
+
+const QuestionStyled = styled.fieldset`
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: auto fr;
+  grid-gap: 12px;
+  border: none;
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  overflow-y: auto;
+  margin: 0;
+
+  legend {
+    display: grid;
+    place-items: center;
+    width: 100%;
+    height: 50vh;
+    font-size: 200%;
+    font-family: serif;
+  }
+
+  label {
+    position: relative;
+    padding: 12px;
+    display: block;
+    display: grid;
+    place-items: center;
+    width: inherit;
+    height: inherit;
+    border: 1px solid;
+    border-radius: 8px;
+    transition: color 2s, background-color 2s;
+  }
+
+  label:focus-within {
+    background-color: black;
+    color: white;
+  }
+
+  input {
+    position: absolute;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+`;
